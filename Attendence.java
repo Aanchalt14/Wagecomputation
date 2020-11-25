671f908515c8d9e9123786a043b30175cb1b1fb8
Aanchalt14 <aanchalt71@gmail.com>
2020-11-26 00:05:08 +0530

    Added : Attendence of employee

---
+++ Attendence.java
@@ -0,0 +1,13 @@
+public class Wagecomputation {
+
+	public static void main(String[] args) {
+    int max=2;
+    int min=1;
+    int a=(int)(Math.random()*(max-min+1)+min);
+    System.out.println (a);
+    if( a==1)
+    System.out.println("Present");
+    else
+    System.out.println("Absent");
+	}
+}
