71e7ee9bccd4dc712a9aba69165d8d8ca946b3a4
Aanchalt14 <aanchalt71@gmail.com>
2020-11-26 01:22:25 +0530

    Added : Part time wage of employee

---
+++ partTimewage.java
@@ -0,0 +1,14 @@
+public class Wagecomputation {
+
+	public static void main(String[] args) {
+   
+    int emp_per_hour=20;
+    int max=2;
+    int min=1;
+    int emphour=8;
+    
+    System.out.println("Emp hours =" +emphour);
+    System.out.println("Part time wage =" +emphour*emp_per_hour);
+	}
+}
+
